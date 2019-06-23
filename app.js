7bbcdfd6b889db6586fe73ce579766e61583184d
chentao <577840797@qq.com>
2019-06-23 18:47:17 +0800

    结构封装

--- app.js
+++ app.js
@@ -1,47 +1,42 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const path = require('path');
+
 //  引入 解析数据的 第三方 模块 
 const bodyParser = require('body-parser');
-// 设置默认采用的模板引擎，名称
-app.set('view enginne', 'ejs');
-// 这是模板页面存放的路径 
-app.set('views', './views');
+
+// 设置 默认采用的模板引擎名称
+app.set('view engine', 'ejs')
+    // 设置模板页面的存放路径
+app.set('views', './views')
 
 // 注册中间件
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 
-// 请求静态托
-app.use('/node_modules', express.static('./node_modules'));
+// 请求静态托管
+app.use('/node_modules', express.static('./node_modules'))
 
-app.get('/', (req, res) => {
-    res.render('index.ejs', { name: 'zs', age: 22 });
+
+// // 导入被抽离出来的没有用户的路由
+// const router1 = require('./router/index.js')
+// app.use(router1);
+// // 导入被抽离的用户逻辑代码的 路由 
+// const router2 = require('./router/user.js')
+// app.use(router2);
+
+
+// 使用循环的的方式 进行路由的自动注册    filenames 是当前文件下所有文件的一个集合
+fs.readdir(path.join(__dirname, './router'), (err, filenames) => {
+    if (err) return console.log('读取router目录中的路由失败！');
+    // 循环router目录下的每一个文件名
+    filenames.forEach(fname => {
+        // 没循环一次 ， 拼接处一个完整的 路由模块 地址
+        // 然后，使用 require 导入这个路由模块 
+        const router = require(path.join(__dirname, './router', fname));
+        app.use(router);
+    });
 });
-
-// 用户请求是注册页面
-app.get('/register', (req, res) => {
-    // 注意 ： 当在调用模板引擎的res.render函数的时候 ./ 相对路径 是相对于 views下面的路径
-    res.render('./user/register.ejs', {});
-});
-
-// 用户请求的是登录页面
-app.get('/login', (req, res) => {
-    res.render('./user/login.ejs', {});
-});
-
-app.post('/register', (req, res) => {
-    /*
-        1.接收前端发送来的post请求信息 
-        2.对前端发送的参数进行 解析 
-        3.对参数 进行校验， 合法性、 重复性
-        4. 往数据库中添加用户名 如果成功 就给一个成功提示 反则给一个错误提示
-
-    */
-    const body = req.body;
-    console.log(body);
-    // 完成用户注册的逻辑
-    res.send({ msg: 'ok', status: 200 });
-});
-
 app.listen(3000, () => {
     console.log('running...');
 });
